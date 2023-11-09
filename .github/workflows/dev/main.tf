af7aa34e11c4b2b7a2bba487fca227e5690a4524
Pavlo Abolmasov <abolmasovp@gmail.com>
2023-11-09 20:21:38 +0200

    Updated terraform files with aws configuration

--- .github/workflows/dev/main.tf
+++ .github/workflows/dev/main.tf
@@ -1,7 +1,18 @@
-resource "null_resource" "test_dev" {
-
+terraform {
+    required_version = ">= 1.3"
+    backend "s3" {
+        bucket = "devops-intern-ilovekharkiv"
+        key = "digger/state.tfstate"
+    }
 }
 
-resource "null_resource" "test_de22d2422" {
+provider "aws" {
+    region = var.region
+}
 
+resource "aws_vpc" "my-vpc" {
+    cidr_block = var.vpc_cidr_block
+    tags = {
+        Name: "${var.env_prefix}-vpc"
+    }
 }
---
+++ .github/workflows/dev/variables.tf
@@ -0,0 +1,3 @@
+variable "env_prefix" {}
+variable "vpc_cidr_block" {}
+variable "region" {}
